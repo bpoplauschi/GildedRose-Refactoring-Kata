31786a1d86d88927c9e853d8007f72c584074f19
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-11-21 10:27:52 +0200

    Updated item "Conjured" to "Conjured Mana Cake" - this was not clear from the specs

--- swift/Sources/GildedRose/ItemQualityCalculatorFactory.swift
+++ swift/Sources/GildedRose/ItemQualityCalculatorFactory.swift
@@ -11,7 +11,7 @@ public func itemQualityCalculatorFactory(item: Item) -> ItemQualityCalculator {
     case "Aged Brie": result = AgedBrieItemQualityCalculator(item: item)
     case "Backstage passes to a TAFKAL80ETC concert": result = TAFKAL80ETCItemQualityCalculator(item: item)
     case "Sulfuras, Hand of Ragnaros": result = SulfurasItemQualityCalculator(item: item)
-    case "Conjured": result = ConjuredItemQualityCalculator(item: item)
+    case "Conjured Mana Cake": result = ConjuredItemQualityCalculator(item: item)
     default: result = AnyItemQualityCalculator(item: item)
     }
     return result
