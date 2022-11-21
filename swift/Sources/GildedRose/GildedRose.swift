ba8112a03b54828a2f71c41ecc8729ed42cfd4b0
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-11-21 09:22:31 +0200

    TAFKAL80ETCItemQualityCalculator

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -82,6 +82,37 @@ final class AgedBrieItemQualityCalculator: ItemQualityCalculator {
     }
 }
 
+final class TAFKAL80ETCItemQualityCalculator: ItemQualityCalculator {
+    private let item: Item
+    
+    init(item: Item) {
+        self.item = item
+    }
+    
+    func updateQuality() {
+        if item.quality < 50 {
+            item.quality = item.quality + 1
+            
+            if item.sellIn < 11 {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+            
+            if item.sellIn < 6 {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+        
+        item.sellIn = item.sellIn - 1
+        
+        if item.sellIn < 0 {
+            item.quality = item.quality - item.quality
+        }
+    }
+}
 
 public class GildedRose {
     var items: [Item]
@@ -95,6 +126,7 @@ public class GildedRose {
             let calculator: ItemQualityCalculator
             switch item.name {
             case "Aged Brie": calculator = AgedBrieItemQualityCalculator(item: item)
+            case "Backstage passes to a TAFKAL80ETC concert": calculator = TAFKAL80ETCItemQualityCalculator(item: item)
             default: calculator = AnyItemQualityCalculator(item: item)
             }
             calculator.updateQuality()
