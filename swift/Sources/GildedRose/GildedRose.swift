1625ac2da6a37e79f5066e9d0ece32c1c71e3fe5
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-11-21 09:30:24 +0200

    Inject factory for creating ItemQualityCalculator into GildedRose

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,4 +1,4 @@
-protocol ItemQualityCalculator {
+public protocol ItemQualityCalculator {
     func updateQuality()
 }
 
@@ -90,21 +90,28 @@ final class SulfurasItemQualityCalculator: ItemQualityCalculator {
 
 public class GildedRose {
     var items: [Item]
+    private let calculatorFactory: (Item) -> ItemQualityCalculator
 
-    public init(items: [Item]) {
+    public init(items: [Item], calculatorFactory: @escaping (Item) -> ItemQualityCalculator = itemQualityCalculatorFactory(item:)) {
         self.items = items
+        self.calculatorFactory = calculatorFactory
     }
     
     public func updateQuality() {
         items.forEach { item in
-            let calculator: ItemQualityCalculator
-            switch item.name {
-            case "Aged Brie": calculator = AgedBrieItemQualityCalculator(item: item)
-            case "Backstage passes to a TAFKAL80ETC concert": calculator = TAFKAL80ETCItemQualityCalculator(item: item)
-            case "Sulfuras, Hand of Ragnaros": calculator = SulfurasItemQualityCalculator(item: item)
-            default: calculator = AnyItemQualityCalculator(item: item)
-            }
+            let calculator: ItemQualityCalculator = calculatorFactory(item)
             calculator.updateQuality()
         }
     }
+}
+
+public func itemQualityCalculatorFactory(item: Item) -> ItemQualityCalculator {
+    let result: ItemQualityCalculator
+    switch item.name {
+    case "Aged Brie": result = AgedBrieItemQualityCalculator(item: item)
+    case "Backstage passes to a TAFKAL80ETC concert": result = TAFKAL80ETCItemQualityCalculator(item: item)
+    case "Sulfuras, Hand of Ragnaros": result = SulfurasItemQualityCalculator(item: item)
+    default: result = AnyItemQualityCalculator(item: item)
+    }
+    return result
 }
